074eec82a63326adc16d2dca0fdbf6db61c1ac8a
WWPPWWP <99242406+WWPPWWP@users.noreply.github.com>
2023-07-18 16:05:10 +0800

    Update BJTUweb.bat

--- BJTUweb.bat
+++ BJTUweb.bat
@@ -1,18 +1,20 @@
 @echo off
 color f0
 
-set username=19111111
-set password=666666
+set username=19111039
+set password=ddrrxx
 
 title BJTUweb-%username%
 
 :Pre
-ping -n 1 10.10.42.3 >nul 2>nul
+echo %date:~0,10% %time:~0,8%  开始检测网络状态
+ping -n 1 10.10.43.3 >nul 2>nul
 if errorlevel 1 goto Exit
 if errorlevel 0 goto Begin
 
 :Exit
-echo %date:~0,10% %time:~0,8%  未接入校园网，不可认证
+echo %date:~0,10% %time:~0,8%  ×无法访问10.10.43.3
+set delaytime=100
 goto Delay
 
 :Begin
@@ -21,14 +23,27 @@ if errorlevel 1 goto Reboot
 if errorlevel 0 goto Continue
 
 :Continue
-echo %date:~0,10% %time:~0,8%  已接入校园网，已认证，无需再次认证
+echo %date:~0,10% %time:~0,8%  √已登录校园网
+set delaytime=1000
 goto Delay
 
 :Reboot
-echo %date:~0,10% %time:~0,8%  已接入校园网，但尚未认证，尝试自动认证
-start msedge "http://10.10.43.3/drcom/login?callback=dr1610523868502&DDDDD=%username%&upass=%password%&0MKKey=123456"
+echo %date:~0,10% %time:~0,8%  ×未登录校园网
+curl -d "DDDDD=%username%&upass=%password%&0MKKey=" http://10.10.43.3 >nul
+ping -n 1 www.baidu.com >nul 2>nul
+if errorlevel 1 goto Fail
+if errorlevel 0 goto Success
+
+:Fail
+echo %date:~0,10% %time:~0,8%  ×登录失败（%username%）
+set delaytime=100
+goto Delay
+
+:Success
+echo %date:~0,10% %time:~0,8%  √登录成功（%username%）
+set delaytime=1000
 goto Delay
 
 :Delay
-timeout /t 200
+timeout /t %delaytime%
 goto Pre
